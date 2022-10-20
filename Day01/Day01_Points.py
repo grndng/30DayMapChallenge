454c5f20fbb2ad383052b51208a08bcc6da5c32a
grndng <gueren.dinga@hcu-hamburg.de>
2022-10-20 19:58:15 +0200

    Had to update my script because there are more weird French territories...

--- Day01/Day01_Points.py
+++ Day01/Day01_Points.py
@@ -10,7 +10,7 @@ the part of France that is geographically in Europe). The same
 applies to Hamburg which has an exclave called "Neuwerk" close
 to the North Sea.
 """
-# tahiti not included...
+
 import geopandas as gpd
 import pandas as pd
 from shapely.geometry import Point
@@ -19,7 +19,18 @@ from keplergl import KeplerGl
 df = gpd.read_file("data/ne_10m_admin_0_countries.shp")
 
 france_complete = df[df["NAME"] == "France"]
+# francentroid = Point(france_complete["geometry"].centroid.x, france_complete["geometry"].centroid.y)
+
+# Okay, so I specifically checked if all overseas territories of
+# France are included because the centroid didn't look right and I
+# could not find French Polynesia. While doing so I've noticed that
+# there seems to be "French Southern Antarctic Lands"... Need to
+# add those too, right?
+
+france_complete = df[df["NAME"].str.startswith("Fr")]
+print(france_complete["NAME"])
 francentroid = Point(france_complete["geometry"].centroid.x, france_complete["geometry"].centroid.y)
+
 
 # At this point: kill me for doing it with keplergl
 df = pd.DataFrame(
@@ -31,3 +42,4 @@ map = KeplerGl()
 map.add_data(data=df, name='Country')
 map.add_data(data = france_complete, name = "France")
 map.save_to_html(file_name = "test.html")
+
