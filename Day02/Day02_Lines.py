3a1f10b9c8b46196afc76b95c4687199a5049911
grndng <gueren.dinga@hcu-hamburg.de>
2022-10-28 09:05:17 +0200

    added file for postcodes

--- Day02/Day02_Lines.py
+++ Day02/Day02_Lines.py
@@ -1,2 +1,6 @@
 # Zip Scribble?
 #https://eagereyes.org/zipscribble-maps/united-states
+
+import pandas as pd
+
+plzdf = pd.read_csv("Day02/data/PLZ.csv", names=["postcode", "cityname", "lon", "lat"], header=None, delimiter=";") # https://launix.de/launix/launix-gibt-plz-datenbank-frei/
