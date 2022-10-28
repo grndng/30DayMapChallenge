be11ce1c008d2dc809d2947b870ad6f34c3ad8ed
grndng <gueren.dinga@hcu-hamburg.de>
2022-10-28 11:11:29 +0200

    Changed variable names and readme

--- Day02/Day02_Lines.py
+++ Day02/Day02_Lines.py
@@ -10,13 +10,13 @@ import pandas as pd
 import altair as alt
 import geopandas as gpd
 
-# Loading Dataframe
-zipcodes = pd.read_csv("Day02/data/PLZ.csv", names=["postcode", "cityname", "lon", "lat"], header=None, delimiter=";") # https://launix.de/launix/launix-gibt-plz-datenbank-frei/
+# Loading 
+plz = pd.read_csv("Day02/data/PLZ.csv", names=["postcode", "cityname", "lon", "lat"], header=None, delimiter=";") # https://launix.de/launix/launix-gibt-plz-datenbank-frei/
 
 # adding leading zeroes to postcodes from 
-for i in zipcodes["postcode"]:
+for i in plz["postcode"]:
     if len(str(i)) < 5:
-        zipcodes["postcode"] = zipcodes["postcode"].astype(str).str.pad(width=5, side="left", fillchar="0")
+        plz["postcode"] = plz["postcode"].astype(str).str.pad(width=5, side="left", fillchar="0")
 
 de = gpd.read_file("Day02/data/bl.json")
 
@@ -24,14 +24,14 @@ basemap = alt.Chart(de).mark_geoshape(
     fill = "lightgray",
     stroke = "white"
 ).properties(
-    width=1000,
-    height=800
+    width=1400,
+    height=1000
 )
 
 # from https://nextjournal.com/sdanisch/cartographic-visualization
 # and https://altair-viz.github.io/altair-tutorial/notebooks/09-Geographic-plots.html
 
-postcode_lines = alt.Chart(zipcodes).transform_filter(
+postcode_lines = alt.Chart(plz).transform_filter(
     "-10 < datum.lon && 25 < datum.lat && datum.lat < 60"
 ).transform_calculate(
     "Leading Digit", alt.expr.substring(alt.datum.postcode, 0, 1)
@@ -43,23 +43,26 @@ postcode_lines = alt.Chart(zipcodes).transform_filter(
     color="Leading Digit:N", # N for nominal (discrete unordered category)
     tooltip=["postcode", "cityname", "lat", "lon"]
 ).properties(
-    width=1000,
-    height=800
+    width=1400,
+    height=1000
 )
 
-pts = alt.Chart(zipcodes).transform_calculate(
+pts = alt.Chart(plz).transform_calculate(
     "Leading Digit", alt.expr.substring(alt.datum.postcode, 0, 1)
-).mark_square(
-    size=2, opacity=0.5
+).mark_circle(
+    size=10, opacity=0.5
 ).encode(
     longitude="lon:Q", # Q for quantitative (continuous real-valued quantity)
     latitude="lat:Q", 
     color="Leading Digit:N", # N for nominal (discrete unordered category)
     tooltip=["postcode", "cityname", "lat", "lon"]
+).properties(
+    width=1400,
+    height=1000
 )
 
-
 base_and_lines = basemap + postcode_lines
-base_and_lines = base_and_lines + pts
+with_points = base_and_lines + pts
 base_and_lines.save("Day02/Day02_Lines.html")
 postcode_lines.save("Day02/Day02_Lines_only.html")
+with_points.save("Day02/Day02_with_points.html")
--- README.md
+++ README.md
@@ -5,13 +5,12 @@ Libraries used: pandas | geopandas | shapely | keplergl
 
 For the first day I wanted a quick win (spoiler: didn't work out) to stay motivated which didn't work out as intended. I wanted to show the difference of the centroid of France when considering the overseas areas in comparison to when considering only the mainland. The same applies to Hamburg to some degree due to an exclave called "Neuwerk" close to the North Sea.
 
-
 <p align="center">
-  <img src="./Day01/Day01_Points.png"/>
+  <img src="./Day01/Day01_Points_Closeup.png"/>
 </p>
 
 <p align="center">
-  <img src="./Day01/Day01_Points_Closeup.png"/>
+  <img src="./Day01/Day01_Points.png"/>
 </p>
 
 Data source: Natural Earth Data
