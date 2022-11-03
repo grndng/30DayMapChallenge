b8eea32ff85a2df5832c2a7411a711bd31209f4f
grndng <gueren.dinga@hcu-hamburg.de>
2022-11-03 13:53:36 +0100

    Did day 03

--- Day03/Day03_Polygons.py
+++ Day03/Day03_Polygons.py
@@ -8,8 +8,30 @@ back and taking a look at Europe would make sense, too.
 
 import geopandas as gpd
 import pandas as pd
+from keplergl import KeplerGl
 
-gpg_df = gpd.read_file("./Day03/data/gender_pay_gap_eurostat.csv")
+# NUR 2020 nehmen!
+
+gpg_df = pd.read_csv("./Day03/data/gender_pay_gap_eurostat.csv")
 eu_df = gpd.read_file("./Day03/data/europe.json")
 
-print(gpg_df)
+gpg_df.rename(columns = {"geo":"name_long", "TIME_PERIOD":"year", "OBS_VALUE":"gap"}, inplace = True)
+
+# long_names = sorted([name for _, name in eu_df["name_long"].items()])
+# shorthands = sorted(list(set([name for name in gpg_df["name_long"]])))
+
+merged_df  = eu_df.merge(gpg_df, on="name_long", how="left")
+merged_gdf = gpd.GeoDataFrame(merged_df)
+
+filter_2020 = merged_gdf["year"] == 2020
+merged_gdf = merged_gdf.loc[filter_2020]
+merged_gdf["gap"] = merged_gdf["gap"].fillna(-9999)
+
+mean_gap = merged_gdf["gap"].mean()
+
+for _, i in merged_gdf["gap"].items():
+    print(mean_gap-i)
+
+map = KeplerGl()
+map.add_data(data=merged_gdf, name="GDF")
+map.save_to_html(file_name = "Day03_Polygons.html")
